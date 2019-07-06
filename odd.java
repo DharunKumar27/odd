02669891cfa12e915697419cb12c090fe6e90fc4
DharunKumar27 <52065228+DharunKumar27@users.noreply.github.com>
2019-07-06 05:55:41 +0530

    Update and rename README.md to odd.java

---
+++ odd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class odd{
+public static void main(String args[])
+{
+Scanner tt=new Scanner(System.in);
+int a;
+System.out.println("enter the value:");
+a=tt.nextInt();
+if(a%2==0)
+{
+System.out.println("even");
+}else
+{
+System.out.println("odd");
+}
+}
+}
